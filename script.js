bc2fc7ab34fc962e8198c21e83e00a5ed7eb528c
Taras Sinyakov <tarikchelli@gmail.com>
2023-02-19 17:50:17 +0200

    type data

---
+++ script.js
@@ -0,0 +1,77 @@
+'use strict'
+let name = 'Ivan';
+let admin = 'Ivan';
+
+// alert(admin)
+
+let planet = 'Earth';
+let currentUser = 'Farkuad'
+
+
+let nameUser = 'Ivan'
+const age = 30
+var job = 'driver'
+
+
+
+// let a = 10
+// console.log(a);
+// a = 20
+// console.log(a);
+
+let someText = `Some text for example 'Hello world!!'`
+// alert(someText)
+
+// __________________
+
+// let numberValue = 34
+// alert(typeof numberValue);
+
+// let stringValue = '34'
+// alert(typeof stringValue);
+
+// let booleanValue =  true
+// alert(typeof booleanValue);
+
+// let bigIntValue =  23n
+// alert(typeof bigIntValue);
+
+// let undefinedValue 
+// alert(undefinedValue);
+
+// let objValue = {}
+// alert(typeof objValue);
+
+// let nullValue = null
+// alert(nullValue);
+
+// let symbolValue = Symbol('2')
+// alert( typeof symbolValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
